d44d866bb3c8ae23bd7a028773aea4abb056f6cc
zbob889 <zroberts889@gmail.com>
2022-12-09 13:34:09 +0100

    Hopefully allowing CORS on the backend

--- backend/routes/toDoRoutes.js
+++ backend/routes/toDoRoutes.js
@@ -8,6 +8,12 @@ const {
 } = require('../controllers/toDoController');
 const router = express.Router();
 
+// Allow CORS
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 // GET all to-dos
 router.get('/', getToDos);
